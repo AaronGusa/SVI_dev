7ab9eb41ca6316cba910cc63ae66847abb80134a
Goosa2025 <gus21002@byui.edu>
2024-01-19 23:41:04 -0700

    common comps added, early assets gathered

---
+++ src/app/commonelements/commonelements.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commonelements',
+  standalone: true,
+  imports: [],
+  templateUrl: './commonelements.component.html',
+  styleUrl: './commonelements.component.css'
+})
+export class CommonelementsComponent {
+
+}
---
+++ src/app/styleguide/styleguide.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-styleguide',
+  standalone: true,
+  imports: [],
+  templateUrl: './styleguide.component.html',
+  styleUrl: './styleguide.component.css'
+})
+export class StyleguideComponent {
+
+}
