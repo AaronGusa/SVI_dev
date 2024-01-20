e43ce456797716203d4bf5bb758c5c2650126b3a
Goosa2025 <gus21002@byui.edu>
2024-01-20 01:51:06 -0700

    made styleguide home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { StyleguideComponent } from './styleguide/styleguide.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: '', component: StyleguideComponent},
     {path: 'home', component: HomeComponent},
     {path: 'style-guide', component: StyleguideComponent}
 ];
