7fd2d2c02feed8a508b934dc67b832f313cc39b9
Goosa2025 <gus21002@byui.edu>
2024-01-23 20:11:46 -0700

    home page mockup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,5 +10,20 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  SV_testimonies = [
+    {username: "Travis",
+    userpic: "../../assets/images/userpics/Trav.jpeg",
+    testimony: "STELLLLLLLLLLLLLLLAAAAAAAAAAAAAAAAAA"},
+     {username: "Sam",
+    userpic: "https://fireflyautomatix.com/wp-content/uploads/2023/12/FF-TEAM-.jpg",
+    testimony: "Aaron's a real sun of a gong"},
+    {username: "Aaron",
+    userpic: "https://yt3.googleusercontent.com/ytc/AIf8zZT_yxWb7R751Kf_hcRcvtFidRJ2cYFQeGUfmDUm=s160-c-k-c0x00ffffff-no-rj",
+    testimony: "It's pretty great, but it could always be better!"},
+    {username: "Other Trav",
+    userpic: "https://media.licdn.com/dms/image/C5603AQFimJG433p-Tw/profile-displayphoto-shrink_200_200/0/1617823598439?e=2147483647&v=beta&t=H2ONlcHQBfnwHLBrcPwwgKqwYz5Jli75nCdMnZcnkyo",
+    testimony: "STALLA VAY MY GUY!"},
+  ];
+
 
 }
