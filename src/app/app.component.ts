8d938fe493650c6194893db8556f31e364e6f86b
AaronGusa <gus21002@byui.edu>
2024-01-24 00:27:05 -0700

    mobile nav startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './commonelements/header/header.component';
 import { FooterComponent } from './commonelements/footer/footer.component';
+import { NavMobileComponent } from './commonelements/nav-mobile/nav-mobile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterLink, RouterOutlet, HeaderComponent, FooterComponent, NavMobileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
