04911ec4393bf230e7bc32ba81233f6712c5f71c
Goosa2025 <gus21002@byui.edu>
2024-02-09 16:44:50 -0700

    Login and Signup initial setup

--- src/app/commonpages/login/login.component.ts
+++ src/app/commonpages/login/login.component.ts
@@ -3,6 +3,12 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { FormControl, FormGroupDirective, NgForm, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { LoadingComponent } from '../../features/loading/loading.component';
+import { RouterLink } from '@angular/router';
+
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -14,13 +20,26 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule,
+  imports: [
+    MatFormFieldModule, 
+    MatInputModule,
     FormsModule,
-    ReactiveFormsModule],
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    LoadingComponent,
+    RouterLink
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  //Variables
+  hidePass = true;
+  isLoading: Boolean = false;
+  tbd: any = '';
+
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
 
   matcher = new MyErrorStateMatcher()
--- src/app/commonpages/signup-user/signup-user.component.ts
+++ src/app/commonpages/signup-user/signup-user.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup-user',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './signup-user.component.html',
   styleUrl: './signup-user.component.css'
 })
