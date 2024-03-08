87669f2b3b20c9ecf92135492df29182aa611550
AaronGusa <gus21002@byui.edu>
2024-03-08 00:06:48 -0700

    Signup components

--- src/app/commonpages/signup-user/signup-user.component.ts
+++ src/app/commonpages/signup-user/signup-user.component.ts
@@ -1,13 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserService } from '../../app-services';
+import { BusSignComponent } from './bus-sign/bus-sign.component';
+import { UserSignComponent } from './user-sign/user-sign.component';
 
 @Component({
   selector: 'app-signup-user',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, BusSignComponent, UserSignComponent],
   templateUrl: './signup-user.component.html',
   styleUrl: './signup-user.component.css'
 })
-export class SignupUserComponent {
+export class SignupUserComponent implements OnInit {
+  goBusinessSignUp: boolean = false;
+  showBusinessForm: boolean = true;
+  userSubSuccess: boolean = false;
+  userFound: boolean = false;
+  Conditions: boolean[];
+  private clientId: string;
+   _foundUser: any;
+  private _foundUserID: number;
+
+  constructor(private userService: UserService) { }
+  
+  ngOnInit() {
+    this.Conditions = [this.goBusinessSignUp, this.showBusinessForm, this.userSubSuccess];
+    // this.goBusinessSignUp = true;
+    // this.showBusinessForm = true;
+    // this.userSubSuccess = true;
+  }
+
+  goToBusiness(value: boolean) {
+    //console.log('Business Go To: ', value)
+    this.goBusinessSignUp = value;
+    this.Conditions[0] = value;
+
+  }
+
+  async handleUserId(value: string) {
+    console.log('User ID passed from Child: ', value)
+    try {
+      this._foundUser = await this.userService.fetchUser(value);
+      this.userFound = true;
+      this._foundUserID = this._foundUser.u_id;
+      //console.log('User Found: ', this.userFound)
+      //console.log('Found User: ', this._foundUser)
+    } catch (error) {
+      console.log('Error Fetching User: ', error);
+    }
+  }
+
+  handleBusiness2Add(value: boolean) {
+    //console.log('Business 2 Add: ', value)
+    this.showBusinessForm = value;
+    this.Conditions[1] = value;
+  }
+
+  handleUserSuccess(value: boolean) {
+    //console.log('User Success: ', value)  
+    this.userSubSuccess = value;
+    this.Conditions[2] = value;
+  }
+
+  happeningMan() {
+    console.log('goBusinessSignUp: ', this.goBusinessSignUp)
+    console.log('showBusinessForm: ', this.showBusinessForm)
+    console.log('userSubSuccess: ', this.userSubSuccess)
+    console.log('_foundUser: ', this._foundUser)
+    console.log('userFound: ', this.userFound)
+    console.log('clientId: ', this.clientId)
+    console.log('_foundUserID: ', this._foundUserID)
+  }
+
+
 
 }
+
---
+++ src/app/commonpages/signup-user/user-sign/user-sign.component.ts
@@ -0,0 +1,127 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
+import { ServiceService } from '../../../app-services';
+import { BusinessService } from '../../../app-services';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-sign',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-sign.component.html',
+  styleUrl: './user-sign.component.css'
+})
+export class UserSignComponent implements OnInit {
+  @Input() _foundUser: number;
+  services: any[] = [];
+  categories: any[] = [];
+  selectedCategories: number[] = [];
+  filteredServices: any[] = [];
+  isLoading: boolean = false;
+  duration: 1500;
+  busServices: number[] = [];
+
+  //form control
+  bus_contact_form: FormGroup = this._formBuilder.group({busContactCtrl: ['']});
+  bus_add_form: FormGroup = this._formBuilder.group({busAddCtrl: ['']});
+  bus_signup_form: FormGroup
+  busForm: FormGroup;
+  
+
+  constructor(private servService: ServiceService,
+              private _formBuilder: FormBuilder,
+              private busService: BusinessService,
+              private r: Router) {}
+
+  ngOnInit() {
+    this.fetchCategories();
+
+    this.busForm = this._formBuilder.group({
+      b_id: [null],
+      b_name: [null, Validators.required],
+      b_email: [null, [Validators.required, Validators.email]],
+      b_phone: [null],
+      b_website: [null],
+      b_street: [null],
+      b_city: [null],
+      b_state: [null],
+      b_zip: [null],
+      b_active: [null],
+      b_services: [null],
+      b_rating: [null],
+      u_id: [null],
+      created:[null]
+    });
+}
+
+async fetchCategories() {
+  this.isLoading = true;
+  this.categories = await this.servService.fetchCategories().toPromise();
+  this.isLoading = false;
+}
+
+addService2Business(serviceId: number) {
+  if (this.busServices.includes(serviceId)) {
+    this.busServices.splice(this.busServices.indexOf(serviceId), 1);
+  } else {
+    this.busServices.push(serviceId);
+  };
+}
+
+async CompleteBusSign() {
+  try{
+    this.busForm.patchValue({
+      b_id:"tbd",
+      b_active: true,
+      b_rating: -1,
+      u_id: this._foundUser,
+      created: new Date(),
+      services: this.busServices
+    });
+
+    console.log(this.busServices);
+
+    // this.busForm = this._formBuilder.group({
+    //   b_id: b_id_value,
+    //   b_name: this.busForm.get('b_name').value,
+    //   b_email: this.busForm.get('b_email').value,
+    //   b_phone: this.busForm.get('b_phone').value,
+    //   b_website: this.busForm.get('b_website').value,
+    //   b_street: this.busForm.get('b_street').value,
+    //   b_city: this.busForm.get('b_city').value,
+    //   b_state: this.busForm.get('b_state').value,
+    //   b_zip: this.busForm.get('b_zip').value,
+    //   b_active: b_active_v alue,
+    //   b_services: this.busServices,
+    //   b_rating: b_rating_value,
+    //   u_id: u_id_value,
+    //   created: created_value
+    // });
+
+    //console.log('Business Form: ', this.busForm.value);
+
+    this.processBusSignUp();
+
+  } catch (error) {
+    console.error('Error Creating Business: ', error);
+  }
+
+  //this.processBusSignUp();
+} 
+
+async processBusSignUp() {
+  try{
+    //console.log('this.busForm.value: ', this.busForm.value);
+    this.isLoading = true;
+    const posted = await this.busService.postBusiness(this.busForm.value);
+    console.log('Business Posted: ', posted);
+    
+    this.isLoading = false;
+    this.r.navigate(['/login']);  
+  } catch (error) {
+    console.error('Error Posting Business: ', error);
+  }
+}
+
+
+}
