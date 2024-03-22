e9b28fecb3d9df390826a6c5b7d5ba81dba20ae4
AaronGusa <gus21002@byui.edu>
2024-03-22 00:44:58 -0600

    appointment and side menu updates

--- src/app/app-services/app-services.module.ts
+++ src/app/app-services/app-services.module.ts
@@ -1,28 +1,31 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BusinessService } from './business.service';
-import { ImageService } from './image.service';
-import { ReviewService } from './review.service';
-import { ServiceService } from './service.service';
-import { UserService } from './user.service';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+//import { createWorker, encode } from '@squoosh/lib';
 
-
-@NgModule({
-  declarations: [],
-  imports: [CommonModule],
-  providers: [
-    BusinessService,
-    ImageService,
-    ReviewService,
-    ServiceService,
-    UserService
-  ],
-  exports: [
-    BusinessService,
-    ImageService,
-    ReviewService,
-    ServiceService,
-    UserService
-  ]
+@Injectable({
+  providedIn: 'root'
 })
-export class AppServicesModule { }
+export class AppServices { 
+  private appBaseUrl = 'https://stellavibe.onrender.com/appnt/';
+  private appUsrUrl = 'usr/';
+  private appAllUrl = 'a_a/';
+
+  constructor(private http: HttpClient) {}
+
+  getUserApps(u_username: string) {
+    try {
+      console.log(`${this.appBaseUrl}${this.appUsrUrl}${u_username}`)
+      const response: any = this.http.get(`${this.appBaseUrl}${this.appUsrUrl}${u_username}`).toPromise();
+      if(response.acknowledged === true) {
+        return response;
+      } else {
+        console.log('USERNAME APPNTMENT ERROR: ' + response)
+        return response;
+      }
+    } catch (error) {
+      return error;
+    }
+  }
+
+
+}
