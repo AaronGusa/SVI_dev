f7b414e7e06d514594b47bdd05bcbd4ae2ce945c
AaronGusa <gus21002@byui.edu>
2024-03-25 16:28:41 -0700

    bus appointments cleaned up

--- src/app/dashboard/dash-bus/db-appointments/db-appointments.component.ts
+++ src/app/dashboard/dash-bus/db-appointments/db-appointments.component.ts
@@ -4,12 +4,10 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { DatePipe, getLocaleFirstDayOfWeek } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 import { AppointmentService } from '../../../app-services';
 import { BusinessService } from '../../../app-services';
 import { UserService } from '../../../app-services';
 import { ServiceService } from '../../../app-services';
-import { firstValueFrom } from 'rxjs';
 import { LoadingComponent } from '../../../features/loading/loading.component';
 
 
@@ -57,7 +55,7 @@ export class DbAppointmentsComponent implements OnInit {
       .subscribe({
         next: categories => {
           this.services = categories;
-          console.log(this.services)
+          //console.log(this.services)
         },
         error: error => {
           // Handle error
@@ -113,13 +111,13 @@ export class DbAppointmentsComponent implements OnInit {
         const service = this.services.find(service => service.s_id === appointment.s_id);
         if (service) {
           this.appointments[index].s_name = service.s_name; // Assign s_name to the specific appointment object
-          console.log(service.s_name);
+          //console.log(service.s_name);
         }
       });
     } else {
       console.error("Services are not properly initialized or fetched.");
     }
-    console.log("S_name added? " + this.appointments);
+    //console.log("S_name added? " + this.appointments);
   }s
 
 
@@ -135,7 +133,6 @@ export class DbAppointmentsComponent implements OnInit {
       }
     });
 
-    this.has_past_appointments = this.pastAppointments.length > 0;
     this.has_current_appointments = this.currentAppointments.length > 0;
   }
 
@@ -185,185 +182,3 @@ export class DbAppointmentsComponent implements OnInit {
     console.log(this.currentAppointments[index]);
   }
 }
-
-// import { Component, OnInit } from '@angular/core';
-// import {MatExpansionModule} from '@angular/material/expansion';
-// import {MatButtonModule} from '@angular/material/button';
-// import {MatSnackBarModule} from '@angular/material/snack-bar';
-// import { DatePipe, getLocaleFirstDayOfWeek } from '@angular/common';
-// import { ActivatedRoute } from '@angular/router';
-// import { HttpClient } from '@angular/common/http';
-// import { AppointmentService } from '../../../app-services';
-// import { BusinessService } from '../../../app-services';
-// import { UserService } from '../../../app-services';
-// import { firstValueFrom } from 'rxjs';
-// import { LoadingComponent } from '../../../features/loading/loading.component';
-
-
-// @Component({
-//   selector: 'app-db-appointments',
-//   standalone: true,
-//   imports: [MatExpansionModule, MatButtonModule, MatSnackBarModule, DatePipe, LoadingComponent ],
-//   templateUrl: './db-appointments.component.html',
-//   styleUrl: './db-appointments.component.css'
-// })
-// export class DbAppointmentsComponent {
-//   username: any = ''; 
-//   userProf: any;
-//   busProf: any;
-//   isLoading: Boolean = true;
-
-//   b_id: string;
-//   appointments: any; 
-//   currentAppointments: any = [];
-//   pastAppointments: any = [];
-//   has_current_appointments: boolean = false;
-//   has_past_appointments: boolean = false;
-//   cancelConfirm: boolean = false;
-//   indexNumber: number = null;
-//   delIndexNumber: number = null;
-//   deleteConfirm: boolean = false;
-
-//   showConfirmation: { [key: string]: boolean } = {};
-
-// constructor(private r: ActivatedRoute,
-//             private aServe: AppointmentService,
-//             private bServe: BusinessService,
-//             private uServe: UserService
-//   ) {}
-
-//   async ngOnInit() {
-//     this.isLoading = true;
-//     this.has_current_appointments = false;
-//     this.has_past_appointments = false;
-//     this.username = this.r.parent.parent.snapshot.paramMap.get('clientUsername');
-//     await this.getClientProfile(); // Wait for getClientProfile to finish before proceeding
-//   }
-  
-//   async getClientProfile() {
-//     this.userProf = await this.uServe.fetchUsername(this.username); // Wait for fetchUsername to resolve
-    
-//     if (this.userProf.u_id) {
-//       await this.getBusB_id(this.userProf.u_id); // Now call getBusB_id after userProf is set
-//     } else {
-//       console.log('UserProf delayed');
-//     }
-//     this.isLoading = false;
-//   }
-  
-//   async getBusB_id(userID) {
-//     //console.log('UserID passed in: ' + userID)
-//     this.busProf = await this.bServe.fetchUserBusiness(userID);
-    
-//     if (this.busProf.b_id) {
-//       await this.getBusAppointments(this.busProf.b_id)
-//     }
-
-//   }
-
-//   async getClientInfo(appointments) {
-//     for (let i = 0; i < appointments.length; i++) {
-//       const appointment = appointments[i];
-//       const userProfile: any = await this.uServe.fetchUsername(appointment.u_username);
-//       if (userProfile) {
-//         appointment.u_fname = userProfile.u_fname; // Assuming 'u_fname' is the property for user first name
-//         appointment.u_lname = userProfile.u_lname; // Assuming 'u_lname' is the property for user last name
-//       }
-//     }
-//   }
-
-//   async getBusAppointments(b_id) {
-//     this.appointments = await this.aServe.getBusApps(b_id);
-//     await this.getClientInfo(this.appointments);
-//     //console.log('The appointemnts: ' + JSON.stringify(this.appointments));
-//     this.filterAppointments(this.appointments);
-//     //await this.getClientInfo(this.appointments);
-//   }
-  
-//   filterAppointments(appointments) {
-//     const today = new Date();
-    
-
-//     appointments.forEach(appointment => {
-//         const appointmentDate = new Date(appointment.app_date);
-//         if (appointmentDate >= today) {
-//             this.currentAppointments.push(appointment);
-//         } else {
-//             this.pastAppointments.push(appointment);
-//         }
-//     });
-
-//     if (this.pastAppointments.length === 0) {
-//       this.has_past_appointments = false;
-//     } else {
-//       this.has_past_appointments = true;
-//     }
-
-//     if (this.currentAppointments.length === 0) {
-//       this.has_current_appointments = false;
-      
-//     } else {
-//       this.has_current_appointments = true;
-//     }
-//   }
-
-//   viewDetails() {
-
-//   }
-
-//   viewAppointment() {
-    
-//   }
-
-//   deleteAppointment(index: number) {
-//     this.delIndexNumber = index;
-//     this.deleteConfirm = true;
-//   }
-
-//   cancelAppointment(index: number) {
-//     this.indexNumber = index;
-//     this.cancelConfirm = false;
-//   }
-
-//   viewPastAppToggle() {
-//     this.has_past_appointments = !this.has_past_appointments;
-//     // console.log(this.has_past_appointments)
-//   }
-
-//   appointStatus(status: string) {
-//     switch (status) {
-//       case "confirmed":
-//         return "✅"; 
-//       case "pending":
-//         return "⚠️";
-//       case "cancelled":
-//         return "❌";
-//       default:
-//         return "";
-//     } 
-//   }
-
-//   // cancelConfirmation() {
-//   //   this.cancelConfirm = !this.cancelConfirm;
-//   // }
-
-//   cancelConfirmation(index: number) {
-//     // // console.log(panelId);
-//     // // console.log(this.showConfirmation)
-//     // this.showConfirmation[panelId] = !this.showConfirmation[panelId];
-//     this.indexNumber = index;
-//     this.cancelConfirm = true;
-// }
-
-//   deleteRecord(index: number) {
-//     this.delIndexNumber = index;
-//     this.deleteConfirm = false;
-//   }
-
-//   confirmAppointment(index: number) {
-//     console.log(this.currentAppointments[index]);
-//   }
-
-  
-
-// }
